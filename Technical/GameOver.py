dc28404fedaea6cf1041f1af3badb27be136a185
Ashik Gurung <142726061+Ashikgurung61@users.noreply.github.com>
2024-10-13 11:30:05 +0530

    Delete Technical directory

--- Technical/GameOver.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-def gameover():
-    root2 = Tk()
-    root2.title("Game")
-    over_bg = PhotoImage(file="Over.png")
-    back_ov = Label(root2, image=over_bg)
-    back_ov.pack()
-    root2.mainloop()
--- Technical/bride.py
+++
@@ -1,2 +0,0 @@
-def bridge(Timer, Title, Marks, Skip, Next, CountScreen, a, lst): 
-    print(f"Time: {Timer}, Title: {Title}, Marks: {Marks}, SKip: {Skip}, Next: {Next}, Screen_Count: {CountScreen}, Last : {a}, Fun: {lst}")
